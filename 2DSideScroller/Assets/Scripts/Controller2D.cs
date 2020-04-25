14cfd0aa617c1b6c63f7e4db86751775efa41b73
Bryssinck,Robbe R <r.bryssinck@student.fontys.nl>
2020-04-25 23:17:04 +0200

    Initial commit

---
+++ 2DSideScroller/Assets/Scripts/Controller2D.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(BoxCollider2D))]
+public class Controller2D : MonoBehaviour
+{
+    // skinwidth is a small part of the player model on the bottom
+    const float skinWidth = .015f;
+    public int horizontalRayCount = 4;
+    public int verticalRayCount = 4;
+
+    float horizontalRaySpacing;
+    float verticalRaySpacing;
+
+    BoxCollider2D boxcollider;
+    RaycastOrigins raycastOrigins;
+
+    void Start()
+    {
+        boxcollider = GetComponent<BoxCollider2D>();
+    }
+
+    void Update()
+    {
+        UpdateRaycastOrigins();
+        CalculateRaySpacing();
+
+        for (int i = 0; i < verticalRayCount; i++)
+        {
+            Debug.DrawRay(raycastOrigins.bottomLeft + Vector2.right * verticalRaySpacing * i, Vector2.up * -2, Color.red);
+        }
+    }
+
+    void UpdateRaycastOrigins()
+    {
+        Bounds bounds = boxcollider.bounds;
+        bounds.Expand(skinWidth * -2);
+
+        raycastOrigins.bottomLeft = new Vector2(bounds.min.x, bounds.min.y);
+        raycastOrigins.bottomRight = new Vector2(bounds.max.x, bounds.min.y);
+        raycastOrigins.topLeft = new Vector2(bounds.min.x, bounds.max.y);
+        raycastOrigins.topRight = new Vector2(bounds.max.x, bounds.max.y);
+    }
+
+    void CalculateRaySpacing()
+    {
+        Bounds bounds = boxcollider.bounds;
+        bounds.Expand(skinWidth * -2);
+
+        // make sure that there are at least 2 rays
+        horizontalRayCount = Mathf.Clamp(horizontalRayCount, 2, int.MaxValue);
+        verticalRayCount = Mathf.Clamp(verticalRayCount, 2, int.MaxValue);
+
+        horizontalRaySpacing = bounds.size.y / (horizontalRayCount - 1);
+        verticalRaySpacing = bounds.size.x / (verticalRayCount - 1);
+    }
+
+    void Move()
+    {
+        
+    }
+
+    struct RaycastOrigins
+    {
+        public Vector2 topLeft, topRight;
+        public Vector2 bottomLeft, bottomRight;
+    }
+}
